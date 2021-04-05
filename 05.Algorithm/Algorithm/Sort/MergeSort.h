b763741d7e0fa47b2af177ee2948b906da70f7a3
FengJungle <fengjungle@126.com>
2021-04-06 07:57:50 +0800

    add mergeSort algorithm bottom to up

--- 05.Algorithm/Algorithm/Sort/MergeSort.h
+++ 05.Algorithm/Algorithm/Sort/MergeSort.h
@@ -1,9 +1,9 @@
 #ifndef _MERGE_SORT_
 #define _MERGE_SORT_
 
-// 归并排序：额外使用O(n)的空间， 时间复杂度O(nlogn)
+// 锟介并锟斤拷锟津：讹拷锟斤拷使锟斤拷O(n)锟侥空间， 时锟戒复锟接讹拷O(nlogn)
 
-// 将arr[l...mid]和arr[mid+1...r]两部分进行归并
+// 锟斤拷arr[l...mid]锟斤拷arr[mid+1...r]锟斤拷锟斤拷锟街斤拷锟叫归并
 template<typename T>
 void __merge(T arr[], int l, int mid, int r){
 	T *aux = new T[r - l + 1];
@@ -33,7 +33,7 @@ void __merge(T arr[], int l, int mid, int r){
 	delete[] aux;
 }
 
-// 递归使用归并排序，对arr[l...r]的范围进行排序
+// 锟捷癸拷使锟矫归并锟斤拷锟津，讹拷arr[l...r]锟侥凤拷围锟斤拷锟斤拷锟斤拷锟斤拷
 template<typename T>
 void __mergeSort(T arr[], int l, int r){
 	if (l >= r){
@@ -53,15 +53,27 @@ void MergeSort(T arr[], int N){
 	__mergeSort(arr, 0, N - 1);
 }
 
-// 自底向上的归并排序算法
+// 锟皆碉拷锟斤拷锟较的归并锟斤拷锟斤拷锟姐法
 //template<typename T>
 //void mergeSortBU(T arr[], int N){
 //	for (int sz = 1; sz <= N; sz *= 2){
 //		for (int i = 0; i + sz < N; i += sz + sz){
-//			// 对arr[i....i+sz-1]和arr[i+sz...i+2*sz-1]进行归并
+//			// 锟斤拷arr[i....i+sz-1]锟斤拷arr[i+sz...i+2*sz-1]锟斤拷锟叫归并
 //			__merge(arr, i, i + sz - 1, min(i + sz + sz - 1, n - 1);
 //		}
 //	}
 //}
+
+// 鑷簳鍚戜笂褰掑苟鎺掑簭
+void mergeSortBU(int arr[], int N)
+{
+	for (int sz = 1; sz < N; sz += sz)
+	{
+		for (int i = 0; i + sz < N; i += 2 * sz)
+		{
+			__merge(arr, i, i + sz - 1, min(i + 2 * sz - 1, N - 1));
+		}
+	}
+}
 
 #endif // _MERGE_SORT_
