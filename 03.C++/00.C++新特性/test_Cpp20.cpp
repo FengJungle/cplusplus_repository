9705525f1e0414fadedace4b4d72e8ef56e7aef9
FengJungle <fengjungle@126.com>
2021-04-21 02:19:47 +0800

    add c++11 new feature part 2

--- 03.C++/00.C++新特性/test_Cpp20.cpp
+++ 03.C++/00.C++新特性/test_Cpp20.cpp
@@ -1,16 +1,7 @@
 // #include <compare>
 #include <iostream>
- 
-int main() {
-    double foo = 0.0;
-    double bar = 1.0;
- 
-    auto res = foo <=> bar;
- 
-    if (res < 0)
-        std::cout << "foo is less than bar";
-    else if (res == 0)
-        std::cout << "foo and bar are equal";
-    else if (res > 0)
-        std::cout << "foo is greater than bar";
+
+int main()
+{
+    __cplusplus;
 }
